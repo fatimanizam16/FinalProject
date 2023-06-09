184da2a5baf36127278627dec63d8a6016fe671c
fatimanizam <fatimanizam16@gmail.com>
2023-06-08 22:30:20 -0400

    "Changing commit"

--- src/test/java/SeleniumAutomation/Pages/PaymentVerification.java
+++ src/test/java/SeleniumAutomation/Pages/PaymentVerification.java
@@ -18,7 +18,7 @@ public class PaymentVerification {
 
     public void doesOTPFieldEditable(String input){
       driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='iframe-3ds']")));
-        Utils.pauseExecution(5);
+        Utils.pauseExecution(3);
         WebElement otpInput = driver.findElement(By.cssSelector("#otp"));
         otpInput.sendKeys(input);
 //        if(otpInput.isDisplayed()){
@@ -42,11 +42,11 @@ public class PaymentVerification {
         Utils.pauseExecution(5);
         WebElement otpInput = driver.findElement(By.cssSelector("#otp"));
         otpInput.sendKeys(input);
-        if(otpInput.isDisplayed()){
-            otpInput.sendKeys(input);
-        }else{
-            throw new NoSuchElementException("OTP field not found");
-        }
+//        if(otpInput.isDisplayed()){
+//            otpInput.sendKeys(input);
+//        }else{
+//            throw new NoSuchElementException("OTP field not found");
+//        }
         driver.findElement(By.cssSelector("button[value='ok']")).click();
         Utils.pauseExecution(5);
         driver.switchTo().frame("snap-midtrans");
